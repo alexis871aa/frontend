9c31eed87ce04fa73a5ee213a6f4c6fdf958276c
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-05-08 01:23:19 +0300

    Создать начальный проект Блог о веб-разработк и настроить его

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-camera-retro fa-2x"></i> fa-camera-retro
+		</Div>
+	);
+};
