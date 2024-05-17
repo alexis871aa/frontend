fe5e6a905a8ffc0a69f75dbccf1fe749ab00a5d7
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-05-17 20:55:43 +0300

    Реализовать основной компонент блога с роутингом

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Blog } from './blog';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { App } from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<BrowserRouter>
+		<Blog />
+	</BrowserRouter>,
+);
